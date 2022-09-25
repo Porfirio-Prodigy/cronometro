6f50e574766a85891c04cb7e1c700c7a5851b81e
Porfirio-Prodigy <prodigy.porfirio@gmail.com>
2022-09-25 09:47:32 -0300

    Cronometro React Native

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      textCron: 'START'
+    };
+
+    this.timer = null;
+
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  vai(){
+
+    if (this.timer != null) {
+      this.setState({textCron: 'START'});
+      clearInterval(this.timer);
+      this.timer = null;
+    } else {
+      this.setState({textCron: 'STOP'});
+      this.timer = setInterval( () => {
+        this.setState({numero: this.state.numero + 0.1})
+      }, 100)
+    }
+  }
+
+  limpar() {
+    clearInterval(this.timer);
+    this.timer = null;
+    this.setState({textCron: "START"});
+    this.setState({numero: 0.0});
+  }
+
+ 
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image 
+          source={require('./src/images/cronometro.png')}
+          style={styles.cronometro}
+        />
+
+        <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
+
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn} onPress={this.vai}>
+            <Text style={styles.btnTexto} >{this.state.textCron}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+            <Text style={styles.btnTexto}>LIMPAR</Text>
+          </TouchableOpacity>
+        </View>
+        
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef',
+
   },
+  cronometro: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain'
+  },
+  timer: {
+    fontWeight: 700,
+    fontSize: 50,
+    color: '#FFF',
+    marginTop: -135,
+  },
+  btnArea: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 90,
+  },  
+  btnTexto: {
+    margin: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    paddingHorizontal: 32,
+    paddingVertical: 8,
+    backgroundColor: '#FFF',
+    color: '#00aeef',
+    fontWeight: 700
+  },
+
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+# Cronometro
+
+Cronometro Simples Feito em React Native.
+## Autores
+
+- [@Porfirio-Prodigy](https://github.com/Porfirio-Prodigy)
+
+
+## Tecnologias
+
+[![JAVASCRIPT](https://img.shields.io/badge/Study%20Project-JAVASCRIPT-yellow)](https://github.com/topics/javascript)
+
+[![REACT NATIVE](https://img.shields.io/badge/Study%20Project-REACT%20NATIVE-%2361DAFB)](https://github.com/facebook/react-native)
+
